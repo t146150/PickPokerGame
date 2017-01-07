e40404be684039288189d74b46fc9f62d16bbd97
t146150 <dumihi1@gmail.com>
2017-01-07 12:09:46 +0700

    First Commit

---
+++ PickPoker/app/src/main/java/com/beginingandroid/hieu_kenken/pickpoker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.beginingandroid.hieu_kenken.pickpoker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
